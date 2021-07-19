b4468cac5be193f80c0ff88ab6dd815541f5b44e
mcube25 <mcube3567@gmail.com>
2021-07-19 02:08:36 -0700

    fixed http bug

--- add-to-cart/src/app/service/api.service.ts
+++ add-to-cart/src/app/service/api.service.ts
@@ -10,7 +10,7 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   getProduct(){
-    return this.http.get<any>("https://https://fakestoreapi.com/products")
+    return this.http.get<any>("https://fakestoreapi.com/products")
     .pipe(map((res:any)=>{
       console.log(res);
       return res;
